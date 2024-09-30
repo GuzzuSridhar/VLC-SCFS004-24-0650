e797ff0fe3e806be24465fe1e2428bcbd3669a3c
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-09-30 17:16:46 +0530

    add utils to project

--- java samples/src/project/Menu.java
+++ java samples/src/project/Menu.java
@@ -7,13 +7,13 @@ public class Menu {
     public static void main(String[] args) {
         byte choice;
         Scanner opt = new Scanner(System.in);
-        System.out.println("Choose from the options");
+        System.out.println(Utils.ANSI_BLUE + "Choose from the options" + Utils.ANSI_RESET);
         System.out.println("---------------------");
-        System.out.println("1. Open Account");
+        System.out.println(Utils.ANSI_GREEN + "1. Open Account");
         System.out.println("2. View Accounts");
         System.out.println("3. View Account Details");
         System.out.println("4. Perform Transaction");
-        System.out.println("5. Exit");
+        System.out.println("5. Exit" + Utils.ANSI_RESET);
         choice = opt.nextByte();
 
         opt.close();
