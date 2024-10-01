f562220f98e7c2ec42541244a13a4e612cde7ea1
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-01 18:56:18 +0530

    add types to ArrayList

---
+++ java samples/src/arraylist/Types.java
@@ -0,0 +1,13 @@
+package arraylist;
+
+import java.util.ArrayList;
+
+public class Types {
+    public static void main(String[] args) {
+        ArrayList<Integer> salList = new ArrayList<>();
+        ArrayList<Float> floats = new ArrayList<>();
+
+        salList.add(100);
+        floats.add(99.4f);
+    }
+}
