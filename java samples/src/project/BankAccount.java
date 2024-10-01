099c151c804b9ec7f4930c2a3969a71ecafdc781
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-10-01 17:07:02 +0530

    add date demo in project

---
+++ java samples/src/project/BankAccount.java
@@ -0,0 +1,20 @@
+package project;
+
+import java.time.LocalDateTime;
+
+public class BankAccount {
+    int acctId;
+    float bal;
+    LocalDateTime createDate;
+
+    public BankAccount(int acctId, float bal, LocalDateTime createDate) {
+        this.acctId = acctId;
+        this.bal = bal;
+        this.createDate = createDate;
+    }
+
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
+        BankAccount acct1 = new BankAccount(100, 99f, LocalDateTime.now());
+    }
+}
